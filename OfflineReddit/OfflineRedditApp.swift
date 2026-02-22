2ee558bc25db1e3e86ad47ef9cdbbbe8dd393d68
VinMan <vinman@vineetm.com>
2026-02-22 10:23:35 +0000

    Initial Commit

---
+++ OfflineReddit/OfflineRedditApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfflineRedditApp.swift
+//  OfflineReddit
+//
+//  Created by Vineet Manghani on 22/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct OfflineRedditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
